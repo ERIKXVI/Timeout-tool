cdef3104625de277f65821fb6b7dc8eea2ef55b6
ERIKXVI <Erik.fastesson@gmail.com>
2023-10-06 19:40:19 +0200

    idk

--- main.cpp
+++ main.cpp
@@ -1,12 +1,238 @@
+#include <Windows.h>
+#include <CommCtrl.h>
 #include <iostream>
 #include <string>
-#include <Windows.h>
 #include <Psapi.h>
-#include <conio.h>
-#include <fstream>
 #include <vector>
+#include <map>
 
 #pragma comment(lib, "Psapi.lib")
+#pragma comment(lib, "Comctl32.lib")
+
+int selectedProcessIndex = -1;
+
+// Declare these variables globally
+HWND processListView;
+HWND searchBox;
+HWND blockButton;
+HWND unblockButton;
+HWND blockedListView;
+
+LVITEM lvItem;
+DWORD processCount;
+DWORD processes[1024];
+WCHAR processName[MAX_PATH];
+
+std::map<DWORD, std::wstring> blockedProcesses;
+
+// Function declarations
+void PopulateListView();
+DWORD GetProcessIdByName(const std::wstring& processName);
+bool HasVisibleWindow(DWORD processId);
+bool BlockInternetAccess(DWORD processId);
+bool UnblockInternetAccess(DWORD processId);
+void UpdateBlockedListView();
+void HandleSearch(const std::wstring& searchText);
+DWORD GetProcessIdByIndex(int index);
+
+// Custom ListView drawing function (implement this with your custom drawing logic)
+void DrawListViewItem(LPDRAWITEMSTRUCT lpDrawItem) {
+    // Implement the drawing logic for your ListView items here
+    // You can use lvItem and the processName to draw the items
+    // ...
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+    case WM_CREATE: {
+        // Create ListView
+        processListView = CreateWindowW(WC_LISTVIEW, L"",
+            WS_VISIBLE | WS_CHILD | LVS_REPORT | LVS_SINGLESEL,
+            10, 90, 400, 200, hwnd, NULL, NULL, NULL);
+
+        // Create columns for the ListView
+        LVCOLUMN lvc = {};
+        lvc.mask = LVCF_TEXT | LVCF_WIDTH;
+        lvc.cx = 100;
+        lvc.pszText = const_cast<LPWSTR>(L"Process Name");
+        ListView_InsertColumn(processListView, 0, &lvc);
+
+        // Enable custom drawing for the ListView
+        ListView_SetExtendedListViewStyle(processListView, LVS_EX_DOUBLEBUFFER | LVS_EX_FULLROWSELECT);
+
+        // Create TextBox
+        searchBox = CreateWindowW(L"Edit", L"",
+            WS_VISIBLE | WS_CHILD | ES_AUTOHSCROLL,
+            10, 10, 200, 30, hwnd, NULL, NULL, NULL);
+
+        // Create Block Button
+        blockButton = CreateWindowW(L"Button", L"Block Internet Access",
+            WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+            220, 10, 200, 30, hwnd, (HMENU)1, NULL, NULL);
+
+        // Create Unblock Button
+        unblockButton = CreateWindowW(L"Button", L"Unblock Internet Access",
+            WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+            220, 50, 200, 30, hwnd, (HMENU)2, NULL, NULL);
+
+        // Create Blocked ListView
+        blockedListView = CreateWindowW(WC_LISTVIEW, L"",
+            WS_VISIBLE | WS_CHILD | LVS_REPORT,
+            10, 300, 400, 200, hwnd, NULL, NULL, NULL);
+
+        // Create columns for the Blocked ListView
+        lvc.cx = 200;
+        lvc.pszText = const_cast<LPWSTR>(L"Blocked Process");
+        ListView_InsertColumn(blockedListView, 0, &lvc);
+
+        // Populate the ListView with processes
+        PopulateListView();
+
+        break;
+    }
+    case WM_COMMAND: {
+        if (HIWORD(wParam) == BN_CLICKED) {
+            if (LOWORD(wParam) == 1) {
+                // Handle block button click
+                if (selectedProcessIndex != -1) {
+                    DWORD selectedProcessId = GetProcessIdByIndex(selectedProcessIndex);
+                    std::wcout << "Block Internet Access button clicked for Process ID: " << selectedProcessId << std::endl;
+                    if (BlockInternetAccess(selectedProcessId)) {
+                        blockedProcesses[selectedProcessId] = std::wstring();
+                        UpdateBlockedListView();
+                    }
+                }
+            }
+            else if (LOWORD(wParam) == 2) {
+                // Handle unblock button click
+                if (selectedProcessIndex != -1) {
+                    DWORD selectedProcessId = GetProcessIdByIndex(selectedProcessIndex);
+                    std::wcout << "Unblock Internet Access button clicked for Process ID: " << selectedProcessId << std::endl;
+                    if (UnblockInternetAccess(selectedProcessId)) {
+                        blockedProcesses.erase(selectedProcessId);
+                        UpdateBlockedListView();
+                    }
+                }
+            }
+        }
+        break;
+    }
+    case WM_NOTIFY: {
+        LPNMHDR pnmh = (LPNMHDR)lParam;
+        if (pnmh->code == NM_CUSTOMDRAW && pnmh->hwndFrom == processListView) {
+            LPNMLVCUSTOMDRAW lplvcd = (LPNMLVCUSTOMDRAW)lParam;
+            if (lplvcd->nmcd.dwDrawStage == CDDS_PREPAINT) {
+                // Request item-specific notifications for custom drawing
+                return CDRF_NOTIFYITEMDRAW;
+            }
+            else if (lplvcd->nmcd.dwDrawStage == CDDS_ITEMPREPAINT) {
+                // Handle custom drawing for individual items
+                if (lplvcd->nmcd.uItemState & CDIS_SELECTED) {
+                    // Item is selected, set the background color
+                    lplvcd->clrTextBk = RGB(173, 216, 230); // Light blue
+                    return CDRF_NEWFONT;
+                }
+            }
+        }
+        if (pnmh->code == LVN_ITEMCHANGED && pnmh->hwndFrom == processListView) {
+            LPNMLISTVIEW pNMLV = (LPNMLISTVIEW)lParam;
+            if ((pNMLV->uChanged & LVIF_STATE) && (pNMLV->uNewState & LVIS_SELECTED)) {
+                // Update the selected process index when a new item is selected in the ListView
+                selectedProcessIndex = pNMLV->iItem;
+            }
+        }
+        break;
+    }
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+
+    default:
+        return DefWindowProc(hwnd, message, wParam, lParam);
+    }
+
+    return 0;
+}
+
+// Function to get the process ID by index
+DWORD GetProcessIdByIndex(int index) {
+    lvItem.mask = LVIF_PARAM;
+    lvItem.iItem = index;
+    lvItem.iSubItem = 0;
+    ListView_GetItem(processListView, &lvItem);
+    return static_cast<DWORD>(lvItem.lParam);
+}
+
+// Function to populate the ListView with processes
+void PopulateListView() {
+    DWORD cbNeeded;
+    if (EnumProcesses(processes, sizeof(processes), &cbNeeded)) {
+        processCount = cbNeeded / sizeof(DWORD);
+
+        lvItem.mask = LVIF_TEXT | LVIF_PARAM; // Add LVIF_PARAM to store process ID
+        lvItem.cchTextMax = 256;
+
+        for (DWORD i = 0; i < processCount; i++) {
+            DWORD processId = processes[i];
+            HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processId);
+            if (hProcess) {
+                if (GetModuleBaseNameW(hProcess, NULL, processName, sizeof(processName) / sizeof(WCHAR))) {
+                    lvItem.iItem = i;
+                    lvItem.iSubItem = 0;
+                    lvItem.pszText = processName;
+                    lvItem.lParam = static_cast<LPARAM>(processId); // Store the process ID as lParam
+                    ListView_InsertItem(processListView, &lvItem);
+                }
+                CloseHandle(hProcess);
+            }
+        }
+    }
+}
+
+// Function to handle the search functionality
+void HandleSearch(const std::wstring& searchText) {
+    // Clear the selection when searching
+    selectedProcessIndex = -1;
+
+    // Clear the ListView
+    ListView_DeleteAllItems(processListView);
+
+    lvItem.mask = LVIF_TEXT | LVIF_PARAM; // Add LVIF_PARAM to store process ID
+    lvItem.cchTextMax = 256;
+
+    for (DWORD i = 0; i < processCount; i++) {
+        DWORD processId = processes[i];
+        HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processId);
+        if (hProcess) {
+            if (GetModuleBaseNameW(hProcess, NULL, processName, sizeof(processName) / sizeof(WCHAR))) {
+                // Check if the process name contains the search text
+                if (searchText.empty() || wcsstr(processName, searchText.c_str()) != nullptr) {
+                    lvItem.iItem = i;
+                    lvItem.iSubItem = 0;
+                    lvItem.pszText = processName;
+                    lvItem.lParam = static_cast<LPARAM>(processId); // Store the process ID as lParam
+                    ListView_InsertItem(processListView, &lvItem);
+                }
+            }
+            CloseHandle(hProcess);
+        }
+    }
+}
+
+// Function to update the Blocked ListView
+void UpdateBlockedListView() {
+    // Clear the Blocked ListView
+    ListView_DeleteAllItems(blockedListView);
+
+    lvItem.mask = LVIF_TEXT;
+
+    for (const auto& kvp : blockedProcesses) {
+        lvItem.iItem = 0; // You can adjust the index as needed
+        lvItem.iSubItem = 0;
+        lvItem.pszText = const_cast<LPWSTR>(kvp.second.c_str());
+        ListView_InsertItem(blockedListView, &lvItem);
+    }
+}
 
 // Function to check if a process has a visible window (user interface)
 bool HasVisibleWindow(DWORD processId) {
@@ -37,7 +263,8 @@ bool BlockInternetAccess(DWORD processId) {
         int result = _wsystem(powershellCmd.c_str());
 
         return (result == 0);
-    } else {
+    }
+    else {
         std::wcerr << "Skipping background process with ID: " << processId << std::endl;
         return false;
     }
@@ -56,118 +283,65 @@ bool UnblockInternetAccess(DWORD processId) {
     return (result == 0);
 }
 
-// Function to list and display running user-level processes (apps) with visible windows
-void ListRunningUserProcessesWithWindows() {
-    std::wcout << "======================================" << std::endl;
-    std::wcout << "Running User-Level Processes with Windows (Apps):" << std::endl;
-    std::wcout << "======================================" << std::endl;
-
+// Function to get the process ID by process name
+DWORD GetProcessIdByName(const std::wstring& processName) {
     DWORD processes[1024];
-    DWORD needed;
-    if (EnumProcesses(processes, sizeof(processes), &needed)) {
-        const DWORD count = needed / sizeof(DWORD);
-        for (DWORD i = 0; i < count; ++i) {
-            const DWORD processId = processes[i];
-            if (HasVisibleWindow(processId)) {
-                HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processId);
-                if (hProcess != nullptr) {
-                    WCHAR processPath[MAX_PATH];
-                    if (K32GetModuleFileNameExW(hProcess, nullptr, processPath, MAX_PATH)) {
-                        std::wcout << i + 1 << ". Process ID: " << processId << std::endl;
-                        std::wcout << "Path: " << processPath << std::endl;
-                        std::wcout << "--------------------------------------" << std::endl;
+    DWORD cbNeeded;
+    if (EnumProcesses(processes, sizeof(processes), &cbNeeded)) {
+        DWORD processCount = cbNeeded / sizeof(DWORD);
+
+        for (DWORD i = 0; i < processCount; i++) {
+            DWORD processId = processes[i];
+            HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processId);
+            if (hProcess) {
+                WCHAR buffer[MAX_PATH];
+                if (GetModuleBaseNameW(hProcess, NULL, buffer, sizeof(buffer) / sizeof(WCHAR))) {
+                    if (processName == buffer) {
+                        CloseHandle(hProcess);
+                        return processId;
                     }
-                    CloseHandle(hProcess);
                 }
+                CloseHandle(hProcess);
             }
         }
     }
+    return 0;
 }
 
-// Function to list blocked processes and their firewall rules
-void ListBlockedProcesses() {
-    std::wcout << "======================================" << std::endl;
-    std::wcout << "Blocked Processes:" << std::endl;
-    std::wcout << "======================================" << std::endl;
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    const wchar_t CLASS_NAME[] = L"My Window Class";
 
-    std::wstring powershellCmd = L"powershell -Command \"";
-    powershellCmd += L"Get-NetFirewallRule -DisplayName 'Block Outgoing' | Select-Object -Property DisplayName, Description, Program | Format-Table -AutoSize\"";
+    WNDCLASS wc = {};
 
-    FILE* pipe = _wpopen(powershellCmd.c_str(), L"rt");
-    if (pipe) {
-        wchar_t buffer[128];
-        while (fgetws(buffer, sizeof(buffer) / sizeof(wchar_t), pipe)) {
-            std::wcout << buffer;
-        }
-        _pclose(pipe);
-    } else {
-        std::wcerr << "Failed to list blocked processes." << std::endl;
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(
+        0,
+        CLASS_NAME,
+        L"Process Control Window",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 500, 600,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
+
+    if (hwnd == NULL) {
+        return 0;
     }
-}
 
-int main() {
-    while (true) {
-        system("cls");
+    ShowWindow(hwnd, nCmdShow);
 
-        std::wcout << "Options:" << std::endl;
-        std::wcout << "1. List running user-level processes (apps) with windows and block internet access for one" << std::endl;
-        std::wcout << "2. List blocked processes" << std::endl;
-        std::wcout << "3. Unblock internet access for a process" << std::endl;
-        std::wcout << "4. Exit" << std::endl;
-        std::wcout << "Enter your choice: ";
+    MSG msg = {};
 
-        int choice;
-        std::wcin >> choice;
-
-        switch (choice) {
-            case 1: {
-                ListRunningUserProcessesWithWindows();
-                std::wcout << "Enter the number of the process to block internet access for: ";
-                int processNumber;
-                std::wcin >> processNumber;
-                if (processNumber >= 1) {
-                    DWORD processes[1024];
-                    DWORD needed;
-                    if (EnumProcesses(processes, sizeof(processes), &needed)) {
-                        const DWORD count = needed / sizeof(DWORD);
-                        if (processNumber <= static_cast<int>(count)) {
-                            if (BlockInternetAccess(processes[processNumber - 1])) {
-                                std::wcout << "Internet access blocked for the selected process." << std::endl;
-                            } else {
-                                std::wcerr << "Failed to block internet access for the selected process." << std::endl;
-                            }
-                        } else {
-                            std::wcerr << "Invalid process number." << std::endl;
-                        }
-                    }
-                } else {
-                    std::wcerr << "Invalid input." << std::endl;
-                }
-                break;
-            }
-            case 2:
-                ListBlockedProcesses();
-                break;
-            case 3: {
-                std::wcout << "Enter the Process ID to unblock internet access for: ";
-                DWORD processId;
-                std::wcin >> processId;
-                if (UnblockInternetAccess(processId)) {
-                    std::wcout << "Internet access unblocked for the selected process." << std::endl;
-                } else {
-                    std::wcerr << "Failed to unblock internet access for the selected process." << std::endl;
-                }
-                break;
-            }
-            case 4:
-                return 0;
-            default:
-                std::wcerr << "Invalid choice. Please enter a valid option." << std::endl;
-                break;
-        }
-
-        std::wcout << "Press any key to continue...";
-        _getch();
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
 
     return 0;
